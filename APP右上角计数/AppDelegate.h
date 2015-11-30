1e184f4a608d06d5dd5c0eb5b650edd67e3c665b
Administrator <Administrator@administratordemacbook-air.local>
2015-11-30 16:09:03 +0800

    Initial Commit

---
+++ APP右上角计数/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APP右上角计数
+//
+//  Created by Administrator on 15/11/30.
+//  Copyright © 2015年 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APP右上角计数/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  APP右上角计数
+//
+//  Created by Administrator on 15/11/30.
+//  Copyright © 2015年 Administrator. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
